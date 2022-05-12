befba8bacec102325f2d3f1d7c00360ee59e1787
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2022-05-12 23:42:08 +0700

    create a temporary game menu
    - add script and audio
    - update terrain
    - update scene levelmanager

---
+++ Assets/Script/Menu/Backsound1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backsound1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameObject.Find("Backsound on")==null){
+            DontDestroyOnLoad(gameObject); //pindah scane nanti audionya tidak kehpus
+            GetComponent<AudioSource>().Play(); //play musik di scane pertama
+            gameObject.name = "Backsound on";
+        }
+        if(GameObject.Find("Touch on")==null){
+            DontDestroyOnLoad(gameObject); //pindah scane nanti audionya tidak kehpus
+            GetComponent<AudioSource>().Play(); //play musik di scane pertama
+            gameObject.name = "Touch on";
+        }
+    }
+
+    public void SoundVolume(float volume){
+        // update volume sesuai parameternya float
+        GetComponent<AudioSource>().volume = volume; 
+    }
+    
+}
